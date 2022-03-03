d1cfbdb263e7c87b2173bdc164246f9dd8792f8d
aishincp <aishinahmed20@gmail.com>
2022-03-03 12:58:42 +0100

    updating line-break in between

---
+++ My_Project/Trace_SDR_Method/Trace_SDR/Trace_SDR/Helpers.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+
+public class Helpers
+{
+    public Helpers()
+    {
+
+    }
+    /// <summary>
+    /// Implements the well formatted pattern of SDR values from the SP.  
+    /// This effect is taking a sample of sdrs that can be generated from Spatial Pattern and everytime the columns can vary in every set.
+    /// So show the difference where the values are changing, we implement this method
+    /// </summary>
+    public static StringBuilder[] StringifyVector(List<List<int>> vector)
+    {
+        //The count for sdr starting from initial position [0,0]
+        var ptrs = new int[vector.Count];  
+        var output = new StringBuilder[vector.Count];
+
+        while (true)
+        {
+            /// <summary>
+            /// <param name="output">The output SDRs of the Spatial Pooler compute cycle.</param>
+            /// <returns></returns>
+            /// 
+
+            //The index will decide which SDR to take
+            int index = 0;       
+            int minActiveColumn = -1;
+
+            //pointer loops twice for 2 sdrs
+            //on each loop you get the current index of that sdr
+                foreach (var ptr in ptrs)
+            {
+                var sdr = vector[index];  
+                if (ptr > sdr.Count - 1)
+                {
+                    index++;
+                    continue;
+                }
+                var activeColumn = sdr[ptr];
+                if (minActiveColumn == -1)
+                {
+                    minActiveColumn = activeColumn;
+                }
+                else
+                {
+                    if (activeColumn < minActiveColumn)
+                        minActiveColumn = activeColumn;
+                }
+
+                index++;
+            }
+            if (minActiveColumn == -1)
+            {
+                return output;
+            }
+
+            //resetting the index value again
+
+            index = 0; 
+            foreach (var ptr in ptrs)
+            {
+                if (output[index] == null)
+                {
+                    output[index] = new StringBuilder();
+                }
+
+                if (ptr < vector[index].Count && vector[index][ptr] == minActiveColumn)
+                {
+                    output[index].Append(minActiveColumn);
+                    output[index].Append(", ");
+                    ptrs[index]++;
+
+                }
+                else
+                {
+                    output[index].Append("  ");
+                    output[index].Append(", ");
+                }
+
+                index++;
+            }
+        }
+    }
+
+
+
+}
---
+++ My_Project/Trace_SDR_Method/Trace_SDR/Trace_SDR/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+Console.WriteLine("\nHello Folks, Let's make a well-formatted multiple sdr values\n");
+
+/// <summary>
+/// We are implementing the Stringify method using two list of SDR values as string
+/// </summary>
+var list = new List<int> { 51, 76, 87, 113, 116, 118, 122, 152, 156, 163, 179, 181, 183, 186, 188, 190, 195, 210, 214, 224 };  // sdr1 values
+var list1 = new List<int> { 51, 76, 113, 116, 118, 156, 163, 179, 181, 182, 183, 186, 188, 190, 195, 197, 210, 214, 224, 243 };  //sdr2 values
+
+var outputs = Helpers.StringifyVector(new List<List<int>> { list, list1 });
+foreach (var output in outputs)
+{
+    Console.WriteLine(output.ToString());
+}
+
