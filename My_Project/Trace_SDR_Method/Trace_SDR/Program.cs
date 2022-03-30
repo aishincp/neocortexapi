3915df2881a10b994ddad9a55be6f4493bb2a26b
Aishin Abdulla Yoosufali <45165287+aishincp@users.noreply.github.com>
2022-03-30 15:36:30 +0200

    updated line break comments

--- My_Project/Trace_SDR_Method/Trace_SDR/Program.cs
+++ My_Project/Trace_SDR_Method/Trace_SDR/Program.cs
@@ -4,11 +4,20 @@ using System.Collections.Generic;
 Console.WriteLine("\nHello Folks, Let's make a well-formatted multiple sdr values\n");
 
 /// <summary>
-/// We are implementing the Stringify method using two list of SDR as values sdr1 and sdr2 as string
+/// Stores random SDR values from one dimensional matrix
 /// </summary>
+/// <param name="list1">First set of SDR</param>
+/// <param name="list2">Second set of SDR</param>
+/// <returns></returns>
+
 var list = new int[] { 051, 076, 087, 113, 116, 118, 122, 152, 156, 163, 179, 181, 183, 186, 188, 190, 195, 210, 214, 224,}; 
 var list1 = new int[] { 051, 076, 113, 116, 118, 156, 163, 179, 181, 182, 183, 186, 188, 190, 195, 197, 210, 214, 224, 243 };  
 
+/// <summary>
+/// This method implements the well formatted pattern of SDR values from the HTM. 
+/// </summary>
+/// <param name="output"></param>
+/// <returns></returns>
 var output = Helpers.StringifyVector(new List<int[]> { list, list1 });
 
 Console.WriteLine(output);
