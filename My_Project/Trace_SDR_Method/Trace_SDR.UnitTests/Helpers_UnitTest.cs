ecaf26546c751e79cc19029c00989354459c3464
aishincp <aishinahmed20@gmail.com>
2022-03-31 05:38:42 +0200

    comments updated

--- My_Project/Trace_SDR_Method/Trace_SDR.UnitTests/Helpers_UnitTest.cs
+++ My_Project/Trace_SDR_Method/Trace_SDR.UnitTests/Helpers_UnitTest.cs
@@ -18,10 +18,18 @@ namespace Trace_SDR.UnitTests
         public void StringifyTest()
         {
 
+            //First SDR vector
             var list = new int[] { 51, 76, 87 };
+            //Second SDR vector
             var list1 = new int[] { 51, 76, 113 };
+            //Stores the two SDRs
             var output = Helpers.StringifyTraceSDR(new List<int[]> { list, list1 });
 
+            /// <summary>
+            /// This result stores the SDR vectors same as output for the comparison. 
+            /// </summary>
+            /// <param name="expectedResult"></param>
+            /// <returns></returns>
             var expectedResult = new StringBuilder();
             var sdr1 = new StringBuilder();
             sdr1.Append("51, 76, 87,    , ");
@@ -34,7 +42,8 @@ namespace Trace_SDR.UnitTests
 
             Console.WriteLine($"{output}");
             var expectedOutput = "51, 76, 87,    ,  \n51, 76,   , 113,";
-
+            
+            //Tests if both have same values 
             Assert.IsTrue(output == expectedResult.ToString());
            
             //Assert.IsTrue(output == expectedOutput);
