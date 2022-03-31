db24fd35c69749f6c7148321e778021a3ae43d5c
aishincp <aishinahmed20@gmail.com>
2022-03-31 02:02:34 +0200

    Program code slight edit

--- My_Project/Trace_SDR_Method/Trace_SDR/Program.cs
+++ My_Project/Trace_SDR_Method/Trace_SDR/Program.cs
@@ -7,19 +7,12 @@ Console.WriteLine("\nLet's make a well-formatted multiple SDR values\n");
 /// <summary>
 /// Stores random SDR values from one dimensional matrix
 /// </summary>
-/// <param name="list1">First set of SDR</param>
-/// <param name="list2">Second set of SDR</param>
+/// <param name="list">First set of SDR</param>
+/// <param name="list1">Second set of SDR</param>
 /// <returns></returns>
-
-
-var list = new int[] { 51, 76, 87, };
-
-var list1 = new int[] { 51, 76, 113, };
-
 
 var list = new int[] { 051, 076, 087, 113, 116, 118, 122, 152, 156, 163, 179, 181, 183, 186, 188, 190, 195, 210, 214, 224,}; 
 var list1 = new int[] { 051, 076, 113, 116, 118, 156, 163, 179, 181, 182, 183, 186, 188, 190, 195, 197, 210, 214, 224, 243 };  
-
 
 /// <summary>
 /// This method implements the well formatted pattern of SDR values from the HTM. 
@@ -28,12 +21,4 @@ var list1 = new int[] { 051, 076, 113, 116, 118, 156, 163, 179, 181, 182, 183, 1
 /// <returns></returns>
 
 var output = Helpers.StringifyTraceSDR(new List<int[]> { list, list1 });
-
-var output = Helpers.StringifyVector(new List<int[]> { list, list1 });
-
-
 Console.WriteLine(output);
-
-
-//var expectedOutput = "51, 76, 87,    ,  \n51, 76,   , 113,";
-//Console.WriteLine($"{expectedOutput}");
