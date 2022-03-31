ea9c6995802f643933d4edeecbfdb036f8b9d261
aishincp <aishinahmed20@gmail.com>
2022-03-31 02:55:41 +0200

    parameter correction in TraceSDR method

--- My_Project/Trace_SDR_Method/Trace_SDR/Program.cs
+++ My_Project/Trace_SDR_Method/Trace_SDR/Program.cs
@@ -11,8 +11,8 @@ Console.WriteLine("\nLet's make a well-formatted multiple SDR values\n");
 /// <param name="list1">Second set of SDR</param>
 /// <returns></returns>
 
-var list = new int[] { 051, 076, 087, 113, 116, 118, 122, 152, 156, 163, 179, 181, 183, 186, 188, 190, 195, 210, 214, 224,}; 
-var list1 = new int[] { 051, 076, 113, 116, 118, 156, 163, 179, 181, 182, 183, 186, 188, 190, 195, 197, 210, 214, 224, 243 };  
+var list = new int[] { 51, 76, 87, 113, 116, 118, 122, 152, 156, 163, 179, 181, 183, 186, 188, 190, 195, 210, 214, 224,}; 
+var list1 = new int[] { 51, 76, 113, 116, 118, 156, 163, 179, 181, 182, 183, 186, 188, 190, 195, 197, 210, 214, 224, 243 };  
 
 /// <summary>
 /// This method implements the well formatted pattern of SDR values from the HTM. 
